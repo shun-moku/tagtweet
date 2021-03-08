cf2129d79a02c0663e01db1c50e46fed457724c5
shun-moku <prmnmbr13.blue828@gmail.com>
2021-03-08 16:47:48 +0900

    first_or_initialize導入

--- app/models/tweets_tag.rb
+++ app/models/tweets_tag.rb
@@ -10,8 +10,10 @@ class TweetsTag
 
   def save
     tweet = Tweet.create(message: message)
-    tag = Tag.create(name: name)
-
+    # tag = Tag.create(name: name)
+    tag = Tag.where(name: name).first_or_initialize
+    tag.save
+    # first_or_initializeの記述だけでは、レコードを探すだけで終わってしまうため、saveメソッドで保存する
     TweetTagRelation.create(tweet_id: tweet.id, tag_id: tag.id)
   end
 
