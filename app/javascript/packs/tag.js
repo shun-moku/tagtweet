8cbf86ae40f1710bafa965dac3e984e6ed7c5def
shun-moku <prmnmbr13.blue828@gmail.com>
2021-03-08 19:47:25 +0900

    openメソッドを用いてリクエスト定義

--- app/javascript/packs/tag.js
+++ app/javascript/packs/tag.js
@@ -4,6 +4,9 @@ if (location.pathname.match("tweets/new")){
     inputElement.addEventListener("keyup", () => {
       const keyword = document.getElementById("tweets_tag_name").value;
       const XHR = new XMLHttpRequest();
+      // openメソッドの第一引数にHTTPメソッド、第二引数にURL、第三引数には非同期通信であることを示すためにtrue
+      // keywordは5行目でフォームから取得した文字列
+      XHR.open("GET", `search/?keyword=${keyword}`, true);
     });
   });
 };
