8588540911d63f8b24129b61cdafd287d877a86e
shun-moku <prmnmbr13.blue828@gmail.com>
2021-03-08 16:52:56 +0900

    tagモデル修正

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
   has_many :tweet_tag_relations
-  has_many :tags, through: :tweet_tag_relations
+  has_many :tweets, through: :tweet_tag_relations
 
   validates :name, uniqueness: true
 end
