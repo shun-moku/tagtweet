b91d200f8c9bb9ce64971117b275b0bca71c5f6a
shun-moku <prmnmbr13.blue828@gmail.com>
2021-03-08 17:43:59 +0900

    XMLHttpRequestオブジェクトを生成

--- app/javascript/packs/tag.js
+++ app/javascript/packs/tag.js
@@ -3,7 +3,7 @@ if (location.pathname.match("tweets/new")){
     const inputElement = document.getElementById("tweets_tag_name");
     inputElement.addEventListener("keyup", () => {
       const keyword = document.getElementById("tweets_tag_name").value;
-      console.log(keyword);
+      const XHR = new XMLHttpRequest();
     });
   });
 };
